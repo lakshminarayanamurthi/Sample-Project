e6a27637d69a157fbfe14f0c32fe2c40cc81ba86
System Admin <DELL@DESKTOP-CO3KDQ0>
2024-05-14 11:46:41 +0530

    test by LN

--- src/SetofPrograms/EvenorOdd_Number.java
+++ src/SetofPrograms/EvenorOdd_Number.java
@@ -4,11 +4,12 @@ public class EvenorOdd_Number {
 
 	public static void main(String[] args) {
 		int num=4;
-		if(num%2==0) {
+		if(num%2==0) 
+		{
 			
 			System.out.println("Number is Even "+ num);
 	}
-		else if()
+		else if(num%2==1)
 		{
 			System.out.println("Else if condition");
 		}
